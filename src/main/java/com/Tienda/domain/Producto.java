53223bc77952f59d13745ab6e1d33c590569aff7
emilyherrera <emilyherrera@localhost>
2023-07-31 18:20:15 -0600

    Finalizacion semana 11

--- src/main/java/com/Tienda/domain/Producto.java
+++ src/main/java/com/Tienda/domain/Producto.java
@@ -1,3 +1,4 @@
+
 package com.Tienda.domain;
 
 import jakarta.persistence.*;
@@ -8,13 +9,13 @@ import lombok.Data;
 @Entity
 @Table(name = "producto")
 public class Producto implements Serializable {
-
     //version de serializacion
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_producto")
+    
     private Long idProducto;
     private String descripcion;
     private String detalle;
@@ -23,7 +24,6 @@ public class Producto implements Serializable {
     private String rutaImagen;
     private boolean activo;
 
-    
     @ManyToOne
     @JoinColumn(name="id_categoria")
     private Categoria categoria;
