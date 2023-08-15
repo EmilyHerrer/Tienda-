b47c9b7d754be5393f89aaae887a83306c74ab85
emilyherrera <emilyherrera@localhost>
2023-08-15 16:29:20 -0600

    Semana 13

--- src/main/java/com/Tienda/controller/IndexController.java
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.Tienda.controller;
 
 import com.Tienda.dao.UsuarioDao;
 import com.Tienda.domain.Usuario;
+import com.Tienda.service.ProductoService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,34 +11,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class IndexController {
-    
+
     @Autowired
-    UsuarioDao usuarioDao;
-    
+    ProductoService productoService;
+
     @RequestMapping("/")
     public String page(Model model, HttpSession session) {
-        
-  //      String imagen = (String)session.getAttribute("usuarioImagen");
-  //      model.addAttribute("avatar", imagen);
-  
-  // Obtener usuario Loggeado
-//  Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-  
-//  UserDetails user = null;
-  
-// if (principal instanceof UserDetails){
-//     user = (UserDetails) principal;
-//  }
-  
-//  if (user != null){
-//      Usuario usuario = usuarioDao.findByUsername(user.getUsername());
-      
-//    session.setAttribute("Email", usuario.getCorreo());
-//  }
+
+        var productos = productoService.getProductos(true);
+        model.addAttribute("productos", productos);
         return "index";
-   }
-    
+    }
+
 }
